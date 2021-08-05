71fb6da9930acd1134361c84955a775ab18b56e4
Ramana9395 <cherukurivenkat9395@gmail.com>
2021-08-05 16:40:44 +0530

    Branch Added

--- src/test/java/test_Cases/ValidateTitle.java
+++ src/test/java/test_Cases/ValidateTitle.java
@@ -41,7 +41,7 @@ public class ValidateTitle extends base{
 				homepage.clickLoginLink().click();
 				logger.info("Clicked Login Link Successfully");
 		//compare the text from the browser with actual text.- Error..
-		Assert.assertEquals(homepage.getTitle().getText(),"SIGN IN");
+		Assert.assertEquals(homepage.getTitle().getText(),"BRANCH");
 		 logger.info("Successfully validated Text message");
 		 System.out.println("Validate Title Test completed");
 		
